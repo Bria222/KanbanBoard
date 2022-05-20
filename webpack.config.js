d57e365e3fcf608899c93fbff3cf38ef72258727
Bria222 <briantechologies.co.ke@gmail.com>
2022-05-20 13:35:50 +0300

    write tests for the counter

--- webpack.config.js
+++ webpack.config.js
@@ -29,11 +29,12 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(svg|eot|woff|woff2|ttf)$/,
-        loader: 'file-loader',
-        options: {
-          outputpath: 'fonts',
-        },
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
